6e4f0958e2dde85b67b0c8a118c3305b6cda491b
Go088 <139887329+Go088@users.noreply.github.com>
2023-12-20 17:45:25 +0000

    Deploying to gh-pages from @ Go088/goit-js-hw-10@76fd39256a9d87916850bba0fbbec4ff1d50c32c 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{f as o}from"./assets/vendor-992cd48f.js";const r=document.querySelector("#datetime-picker"),e=document.querySelector("button"),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){t[0]<new Date?(window.alert("Please choose a date in the future"),e.setAttribute("disabled",!0)):e.removeAttribute("disabled"),console.log(userSelectedDate)}};o(r,n);
+//# sourceMappingURL=commonHelpers.js.map
