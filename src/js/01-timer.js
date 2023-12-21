87bf6ff44a32a4f1f22fb446b940fd06335443eb
Go088 <139887329+Go088@users.noreply.github.com>
2023-12-21 13:51:43 +0200

    Update 01-timer.js

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -4,23 +4,26 @@ import "flatpickr/dist/flatpickr.min.css";
 const input = document.querySelector("#datetime-picker");
 const button = document.querySelector("button");
 
+let userSelectedDate = "";
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-        if (selectedDates[0] < new Date()) {
-            window.alert("Please choose a date in the future")
-            button.setAttribute("disabled", true);
-        }
-        else {
-            button.removeAttribute("disabled");
-        }
-        console.log(userSelectedDate);
-    },
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert("Please choose a date in the future");
+      button.setAttribute("disabled", true);
+    } else {
+      button.removeAttribute("disabled");
+      userSelectedDate = selectedDates[0];
+    }
+  },
 };
 
 flatpickr(input, options);
+
+console.log(options.onClose);
 
 
