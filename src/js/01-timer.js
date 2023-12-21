be816fbda31197b51dc48586de8514b0b4c7382e
Go088 <139887329+Go088@users.noreply.github.com>
2023-12-21 16:16:49 +0200

    Update 01-timer.js

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -4,8 +4,6 @@ import "flatpickr/dist/flatpickr.min.css";
 const input = document.querySelector("#datetime-picker");
 const button = document.querySelector("button");
 
-let userSelectedDate = "";
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -14,15 +12,42 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
       window.alert("Please choose a date in the future");
-      button.setAttribute("disabled", true);
+      button.setAttribute("disabled", true); return
     } else {
-      button.removeAttribute("disabled");
-      userSelectedDate = selectedDates[0];
+        button.removeAttribute("disabled");
     }
   },
 };
 
-flatpickr(input, options);
+const datePicker = flatpickr(input, options);
+let userSelectedDate = datePicker.selectedDates[0];
+
+let timeDifference = new Date() - datePicker.selectedDates[0];
+
+console.log(timeDifference);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
+
+
+
+
+
 
 
 
